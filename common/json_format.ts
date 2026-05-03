5390a08b549d5038d4d84c74fe2ae2712eeca820
tmp13 <ptmp13@gmail.com>
2026-05-03 18:30:39 +0300

    check hightlight

--- common/json_format.ts
+++ common/json_format.ts
@@ -13,87 +13,25 @@ export class JsonFormat extends FieldFormat {
 	getParamDefaults(): FieldFormatParams {
 		return {}
 	}
-}
 
 	// 4. Implement a conversion function
-  // htmlConvert: HtmlContextTypeConvert = (val,opts) => {
-  //   const { isJson, inDocViewer, value } = getFormattedJson(String(val));
-  //   console.log('inDocViewer: ',inDocViewer);
-  //   console.log('val: ',val);
+  htmlConvert: HtmlContextTypeConvert = (val,opts) => {
+    console.log('val: ',val);
+    const highlights = opts?.hit?.highlight;
+    console.log('Available fields in this opts:', Object.keys(opts?.hit || {}));
+    console.log('highlights:', highlights);
+    if (highlights) {
+      // TypeScript now knows 'highlights' is an object
+      const highlightKeys = Object.keys(highlights);
+      const targetKey = highlightKeys.find(key => key.includes('GGGG'));
 
-  //   // Debug
-  //   // console.log('[json] val:', val);
-  //   // console.log('[json] isJson:', isJson);
-  //   // console.log('[json] inDocViewer:', !!inDocViewer);
-  //   // console.log('[json] flyoutClosed:', flyoutClosed);
+      if (targetKey) {
+        const highlightedValue = highlights[targetKey];
+        console.log('Found highlight:', highlightedValue);
+        // Use highlightedValue here
+      }
+    }
+    return val;
+  }
+}
 
-  //   // If not JSON or not inside flyout → return plain
-  //   if (!isJson) {
-  //     return val;
-  //   }
-  //   return `<pre class="json-field">${syntaxHighlightFormattedJson(value)}</pre>`;
-  // }
-
-	// textConvert: TextContextTypeConvert = (val) => {
-  //   function extracted() {
-  //     // console.log('[json] TextContextTypeConvert:', String(val));
-  //     return getFormattedJson(String(val)).value
-  //   }
-
-  //   return extracted();
-  // }
-// }
-
-// add options for the format to be edited if required
-// const JsonFormatEditor = () => createElement('div', {})
-
-// // 2. Make sure it has a `formatId` that corresponds to format's id
-// JsonFormatEditor.formatId = JsonFormat.id
-
-// // 3. Wrap editor component in a factory. This is needed to support and encourage code-splitting.
-// export const JsonFormatEditorFactory = async() => JsonFormatEditor
-// JsonFormatEditorFactory.formatId = JsonFormatEditor.formatId
-
-// syntax highlighting for JSON strings
-// from ChatGPT
-// function syntaxHighlightFormattedJson(json: string) {
-// 	// Replace specific parts of the JSON with colored spans
-// 	json = json
-// 		.replace(/&/g, '&amp;')
-// 		.replace(/</g, '&lt;')
-// 		.replace(/>/g, '&gt;')
-// 	return json.replace(/("(\\u[\da-fA-F]{4}|\\[^u]|[^\\"])*")(\s*:\s*)?|(\b(true|false|null)\b)|(-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, (match, key, stringVal, colon, literal, booleanOrNull, number) => {
-// 		if(key) {
-// 			// If there's a colon after the string, it's a key
-// 			return '<span class="key">' + key + '</span>' + (colon || '')
-// 		} else if(booleanOrNull) {
-// 			// Booleans and null
-// 			return '<span class="boolean">' + booleanOrNull + '</span>'
-// 		} else if(number) {
-// 			// Numbers
-// 			return '<span class="number">' + number + '</span>'
-// 		}
-
-// 		// Strings
-// 		return '<span class="string">' + match + '</span>'
-// 	})
-// }
-
-/**
- * Returns a formatted JSON string if the input is a valid JSON string,
- * otherwise returns the input string as is.
- * @param value {"abcd":true}
- * @returns {} '{ isJson: true, value: "{\n \"abcd\": true\n}" }'
- */
-// function getFormattedJson(value: string) {
-// 	try {
-// 		const obj = JSON.parse(value)
-// 		return {
-// 			isJson: true,
-//       inDocViewer: true,
-// 			value: JSON.stringify(obj, null, 2)
-// 		}
-// 	} catch{
-// 		return { isJson: false, inDocViewer: false, value }
-// 	}
-// }
